7c3a03aa5d56f39f49e6a74cae2b9e2c9b72e6ab
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-08-15 14:20:36 +0100

    update

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ import figlet from "figlet";
 import gradient from "gradient-string";
 import inquirer from "inquirer";
 import axios from "axios";
-import { observable,from } from "rxjs";
+// import { observable, from } from "rxjs";
+import events from "events";
+
+const emitter = new events.EventEmitter();
 
 let playerName;
 // const sleep = (ms = 2000) => new Promise((r)=>setTimeout(r,ms));
@@ -33,41 +36,6 @@ axios
     `https://the-trivia-api.com/api/questions?categories=general_knowledge&limit=${amountAns}&region=NG&difficulty=${difficultyAns}`
   )
   .then((res) => {
-
-
-    // const observable = from(resObj.response);
-    // const subscription = observable.subscribe(
-    //   x => { inquirer.prompt({
-    //       //suppose to have a variable name of answers
-    //       type: "list",
-    //       name: `current_question`, //observe
-    //       message: x.question,
-    //       choices: [x.correctAnswer,...x.incorrectAnswers],
-    //     });
-    //      handleAnswer(answers.current_question === x.correctAnswer);});
-    // // Later:
-    // subscription.unsubscribe();
-
-    
-      // let observer = {
-      //   next: (value) => {
-      //     console.log(value);
-      //   },
-      //   error: (error) => {
-      //     console.log(error);
-      //   },
-      //   complete: () => {
-      //     console.log("completed");
-      //   },
-      // };
-      // observable
-      //   .create((obs) => {
-      //     obs.next("a val");
-      //   })
-      //   .subscribe(observer);
-
-///////////////////////////////////////////////////////////////////////////////
-
     //  incoming data
     res.data.forEach((opt, index, arr) => {
       let valObj = {
@@ -75,25 +43,36 @@ axios
         question: opt.question,
         correctAnswer: opt.correctAnswer,
         incorrectAnswers: opt.incorrectAnswers,
-        pushOPtionsVal: options.push(
-          this.correctAnswer,
-          ...this.incorrectAnswers
-        ),
-        optionsSort: options.sort(() => 0.5 - Math.random()),
-        emptyOptionsArr: options.splice(0, options.length),
+        pushOPtionsVal: function () {
+          return options.push(opt.correctAnswer, ...opt.incorrectAnswers);
+        },
+        optionsSort: () => {
+          return options.sort(() => 0.5 - Math.random());
+        },
+        emptyOptionsArr() {
+          return options.splice(0, options.length);
+        },
       };
-      let answers = await inquirer.prompt({
-        //suppose to have a variable name of answers
-        type: "list",
-        name: `current_question`, //observe
-        message: question,
-        choices: [],
-      });
-      // check if a question option has been selected
-      if (answers.current_question) {
-        return handleAnswer(answers.current_question === valObj.correctAnswer);
-      }
 
+      // console.log(valObj.question)
+      valObj.pushOPtionsVal();
+      // valObj.optionsSort();
+      // console.log(valObj.correctAnswer)
+      // console.log(options);
+      // valObj.emptyOptionsArr()
+
+      let answers = async () =>{
+         await inquirer.prompt({
+          type: "list",
+          name: `current_question`, //observe
+          message: valObj.question,
+          choices: valObj.optionsSort(),
+        });
+       return handleAnswer(answers.current_question === valObj.correctAnswer);
+      };
+      emitter.on("close", answers())
+
+      
     });
   })
   .catch((err) => {
